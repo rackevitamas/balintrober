a786664bebce1de109821b39f32a4b7fa7aa85c2
szt3rackev <szt3rackev@SZT5-9-HP>
2025-03-14 10:41:58 +0100

    Félig

--- src/stores/appointment.js
+++ src/stores/appointment.js
@@ -11,22 +11,26 @@ export const useAppointmentStore = defineStore('appointment', () => {
 
   const hours = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00'];
 
-  const isFree = [];
-  let rows = 8;
-  let col = 4;
+  const isFree = computed(() =>{
+    let rows = 8;
+    let col = 4;
+    const freeSlots = [];
 
-  for (let i = 0; i < rows; i++) {
-    isFree[i] = [];
-    for (let j = 0; j < col; j++) {
-      if (appointmentStore.value[j] == hours[i]) {
-        isFree[i][j] = 'foglalt';
-      }
-      else
-      {
-        isFree[i][j] = 'szabad';
+    for (let i = 0; i < rows; i++) {
+      if (!freeSlots[i]) freeSlots[i] = [];
+      for (let j = 0; j < col; j++) {
+        if (appointmentStore.value[j] && appointmentStore.value[j].time === hours[i]) {
+          freeSlots[i][j] = 'foglalt';
+        }
+        else
+        {
+          freeSlots[i][j] = 'szabad';
+        }
       }
     }
-  }
+    return freeSlots;
+  });
+  
 
-  return { appointmentStore, fetchAppointments, isFree }
+  return { appointmentStore, fetchAppointments, hours, isFree }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
   import { useAppointmentStore } from '@/stores/appointment';
 
   const store = useAppointmentStore();
+
   onMounted(async () => {
     await store.fetchAppointments();
   })
@@ -12,24 +13,28 @@
 <template>
   <div>
     <table>
-      <tr>
-      <th class="border" style="width: 13%;"></th>
-      <th class="border">Hétfő</th>
-      <th class="border">Kedd</th>
-      <th class="border">Szerda</th>
-      <th class="border">Csütörtök</th>
-      <th class="border">Péntek</th>
-    </tr>
+      <thead>
+        <tr>
+          <th class="border" style="width: 13%;"></th>
+          <th class="border">Hétfő</th>
+          <th class="border">Kedd</th>
+          <th class="border">Szerda</th>
+          <th class="border">Csütörtök</th>
+          <th class="border">Péntek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(h, index) in store.hours" :key="index">
+          <td class="border">{{ h }}</td>
+        </tr>
+
+        <tr v-for="(row, rowIndex) in store.isFree" :key="rowIndex">
+          <td v-for="(status, colIndex) in row" :key="colIndex">
+            <p v-if="status === 'szabad'" class="btn btn-dark">{{ status }}</p>
+            <button v-else class="btn btn-danger" disabled>{{ status }}</button>
+          </td>
+        </tr>
+      </tbody>
     </table>
-    <table>
-    <tr v-for="h in store.hours">
-      <td class="border">{{ h }}</td>
-    </tr>
-    <tr v-for="[d, h] in store.isFree">
-      <td v-if="h == 'szabad'"><p class="btn btn-dark">{{ h }}</p></td>
-      <td v-else><button class="btn btn-danger" disabled>{{ h }}</button></td>
-    </tr>
-    
-  </table>
   </div>
 </template>
